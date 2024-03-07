f8e8cffe31b505b1941f90c65bec2cc08257586b
Ziad Omar <61595704+Ezay06@users.noreply.github.com>
2024-03-07 06:21:36 -0800

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import random
-counter = 0
-
-upper = int(input("Enter the max range: "))
-lower = int(input("Enter the min range: "))
-num = random.randint(lower, upper)
-guess = str(input('Is '+str(num)+' the number you were thinking of?: '))
-
-while guess != 'Exit' or 'exit':
-    counter += 1
-
-    if guess == 'too high':
-        num = random.randint(lower, num-1)
-        guess = str(input('Is '+str(num)+' the number you were thinking of?: '))
-
-    elif guess == 'too low':
-        num = random.randint(num+1, upper)
-        guess = str(input('Is '+str(num)+' the number you were thinking of?: '))
-
-    elif guess == 'correct':
-        print('It took from the program ', counter, ' times to guess right.')
-        y = str(input('Would you like to play again?: '))
-
-        if y == 'yes':
-            counter = 0
-            upper = int(input('Enter the max range: '))
-            lower = int(input('Enter the min range: '))
-            num = random.randint(lower, upper)
-            guess = str(input('Is '+str(num)+' the number you were thinking of?:  '))
-        else:
-            break
-
-    else:
-        print('Error! You typed wrong. Please try again.')
-        counter = counter-1
-        guess = str(input('Is '+str(num)+' the number you were thinking of?: '))
-print('Thanks for playing')
-=======
 import random
 print('''
       Welcome to Ziad's guessing game. The rules are simple:
@@ -113,4 +74,3 @@ def guessing_game():
 
 
 guessing_game()
->>>>>>> 9bee5a935db4b50cd8d8f3b0edcfe50c067b3e3d
